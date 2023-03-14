d2925031293e44a1ff2cefa6687d8316633b2323
Vitor <pro.vitoralmeida@gmail.com>
2023-03-14 16:01:59 -0300

    Adiciona leitura das informações necessárias por variáveis de ambiente e cria o segredo novo com base no antigo

--- readenv.py
+++ readenv.py
@@ -3,6 +3,10 @@ import time
 
 while(True):
     secret = os.getenv('SECRET')
+    env1 = os.getenv('ENV1')
+    env2 = os.getenv('ENV2')
     time.sleep(2)
     print(secret)
+    print(env1)
+    print(env2)
 
--- secretsupdate.py
+++ secretsupdate.py
@@ -2,54 +2,98 @@ from kubernetes import client, config
 import time
 import hvac
 import base64
+import os
 
-
-
-def restart_pod():
+def restart_pod(app_name, namespace):
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
     print("Listing pods with their IPs:")
     ret = v1.list_pod_for_all_namespaces(watch=False)
+    print(ret.items)
     time.sleep(10)
     for i in ret.items:
-        if "python-readenv" in i.metadata.name:
+        if app_name in i.metadata.name:
             print("deleting pod %s\t%s\t%s" %
                   (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-            v1.delete_namespaced_pod(name=i.metadata.name, namespace="vault", body=client.V1DeleteOptions())
+            v1.delete_namespaced_pod(name=i.metadata.name, namespace=namespace, body=client.V1DeleteOptions())
 
 
-def get_password():
-    client = hvac.Client(url='http://10.244.1.85:8200',  token="root")
-    print("conectou com o vault")
+def get_password(vault_addr, vault_secret_path, vault_token):
+    client = hvac.Client(url=f'{vault_addr}:8200',  token=vault_token)
+
+#    Habilitar approle
+
+#    client = hvac.Client()
+#
+#    client.sys.enable_auth_method(
+#        method_type='approle',
+#    )
+#
+#    # Mount approle auth method under a different path:
+#    client.sys.enable_auth_method(
+#        method_type='approle',
+#        path='my-approle',
+#    )
+
+#    Authenticação hvac.api.auth_methods.AppRole.login()
+
+#    client = hvac.Client()
+#
+#    client.auth.approle.login(
+#        role_id='<some_role_id>',
+#        secret_id='<some_secret_id>',
+#    )
+
+
+    print("vault connected")
     print(client.is_authenticated())
-    read_response = client.secrets.kv.read_secret_version(path='poc')
+    print("getting pass in vault")
+    read_response = client.secrets.kv.read_secret_version(path=vault_secret_path)
     return read_response
 
-def update_secret(password):
+
+def update_secret(password, namespace, app_name):
     time.sleep(10)
     config.load_incluster_config()
     api_instance = client.CoreV1Api()
     sec = client.V1Secret()
-    print("deleting secret...")
-    api_instance.delete_namespaced_secret(name="python-readenv", namespace="vault", body=sec)
-    print("creating secret...")
-    print(f"SENHA {password}")
-    sec.metadata = client.V1ObjectMeta(name="python-readenv")
+    print("reading secret")
+    secret = api_instance.read_namespaced_secret(name=app_name, namespace=namespace)
+    print(secret.data)
+    print(f"updating secret instace with new password {password}")
+    secret.data['secret'] = password
+    print(secret.data)
+    print("deleting old secret...")
+    api_instance.delete_namespaced_secret(name=app_name, namespace=namespace, body=sec)
+    print("recreating secret...")
+    sec.metadata = client.V1ObjectMeta(name=app_name)
     sec.type = "Opaque"
-    sec.data = {"secret": password }
-    api_instance.create_namespaced_secret(namespace="vault", body=sec)
+    sec.data = secret.data
+    api_instance.create_namespaced_secret(namespace=namespace, body=sec)
     restart_pod()
 
 def main():
-    print("buscando password no vault")
-    password = get_password()
-    print(password['data']['data']['pass'])
-    password = password['data']['data']['pass']
+    vault_addr = os.getenv('VAULT_ADDR').strip()
+    vault_token = os.getenv('VAULT_TOKEN').strip()
+    vault_secret_path = os.getenv('VAULT_SECRET_PATH').strip()
+    vault_secret_key = os.getenv('VAULT_SECRET_KEY').strip()
+    app_name = os.getenv('APP_NAME').strip()
+    namespace = os.getenv('NAMESPACE').strip()
+    print(vault_addr)
+    print(vault_token)
+    print(vault_secret_path)
+    print(app_name)
+    print(namespace)
+
+    password = get_password(vault_addr, vault_secret_path, vault_token)
+    print(password)
+    print(password['data']['data'][vault_secret_key])
+    password = password['data']['data'][vault_secret_key]
     password = base64.b64encode(password.encode('ascii'))
     print(password)
     print(password.decode('UTF-8'))
-    update_secret(password.decode('UTF-8'))
+    update_secret(password.decode('UTF-8'), namespace, app_name)
 
 if __name__ == '__main__':
     main()
